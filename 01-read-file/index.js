6876eae5a269d03ab7fb8f4a0e7c9efd34d06f1c
SpacePocket <space1985pocket@hmail.com>
2023-05-03 23:51:34 +0300

    refactor: update solution for task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,7 @@
 const path = require('path');
 const fs = require('fs');
-console.log(path.join(__dirname, '01-read-file', 'text.txt'))
-const stream = fs.createReadStream(path.join(__dirname, '01-read-file', 'text.txt'), 'utf-8');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 let data = '';
-
-stream.on(path.join(__dirname, '01-read-file', 'text.txt'), chunk => data += chunk);
-stream.on('end', () => console.log('End', data));
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => console.log('End of file reading. Your data is  -> ', data));
 stream.on('error', error => console.log('Error', error.message));
-
-
