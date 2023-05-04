e41d7ce0f2d828ea6f7f48b251a5b366018a4aa5
Space Pocket <space1985pocket@gmail.com>
2023-05-04 09:56:11 +0300

    refactor: update solutions for first and second tasks

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf-8');
 let data = '';
-stream.on('data', chunk => data += chunk);
-stream.on('end', () => console.log('End of file reading. Your data is  -> ', data));
-stream.on('error', error => console.log('Error', error.message));
+readStream.on('data', chunk => data += chunk);
+readStream.on('end', () => console.log('End of file reading. Your data is  -> ', data));
+readStream.on('error', error => console.log('Error', error.message));
+
---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+
+const { stdout, stdin, exit } = process;
+const path = require('path');
+const fs = require('fs');
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath, 'utf-8');
+
+stdout.write(' ----->  Hello. Please enter your text. After you can type "exit" to save it <------\n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    stdout.write(' -----> See you later! Goodbye! <------  ')
+    exit();
+  }
+  writeStream.write(data);
+});
+
+process.on('SIGINT', exit);
