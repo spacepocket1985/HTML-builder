f9dd6d369692ec4f383572548726a05e2bba4e17
SpacePocket <space1985pocket@hmail.com>
2023-05-08 22:53:49 +0300

    refactor: update task solutions 01-02

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -4,6 +4,6 @@ const filePath = path.join(__dirname, 'text.txt');
 const readStream = fs.createReadStream(filePath, 'utf-8');
 let data = '';
 readStream.on('data', chunk => data += chunk);
-readStream.on('end', () => console.log('End of file reading. Your data is  -> ', data));
+readStream.on('end', () => console.log('End of file reading. Your data is  ->\n'+ data));
 readStream.on('error', error => console.log('Error', error.message));
 
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -10,14 +10,14 @@ const sayGoodBuy = () => {
   stdout.write('-----> See you later! Goodbye! <------  ');
   process.exit();
 };
-stdout.write('----->  Hello. Please enter your text. <------\n');
+stdout.write('----->  Hello. Please enter your text. ------>\n');
 
 stdin.on('data', (data) => {
   if (data.toString().trim() === 'exit') {
     sayGoodBuy();
   }
   writeStream.write(data);
-  stdout.write(`-----> Yor text '${data.toString().trim()}' has been added to the file "${fileName}" <------ You can type "exit" to save it\n`);
+  stdout.write(`---> Yor text '${data.toString().trim()}' has been added to the file "${fileName}" <---- You can type "exit" to save it\n`);
 });
 
 process.on('SIGINT', sayGoodBuy);
